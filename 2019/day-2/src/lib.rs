4b862db1caf98bc6e555654a27fdf50585b2a3e6
Amy Jie <git.xvrqt.com>
2019-12-06 11:34:34 -0800

    Further cleaned up the examples

--- 2019/day-2/src/lib.rs
+++ 2019/day-2/src/lib.rs
@@ -1,6 +1,7 @@
 /* Intcode Computer */
 pub struct IntcodeMachine {
     pc: usize,
+    bin: Vec<usize>,
     mem: Vec<usize>,
     instr_ptr: usize,
 }
@@ -13,9 +14,11 @@ pub enum OPCODE {
 }
 
 impl IntcodeMachine {
-    pub fn new(mem: Vec<usize>) -> Self {
-        IntcodeMachine { 
+    pub fn new(bin: Vec<usize>) -> Self {
+        let mem = bin.clone();
+        IntcodeMachine {
             pc: 0,
+            bin,
             mem,
             instr_ptr: 0,
         }
@@ -29,18 +32,20 @@ impl IntcodeMachine {
                     let (input1, input2, output_ptr) = self.get_full_opcode()?;
                     self.mem[output_ptr] = input1 + input2;
                     Ok(OPCODE::ADD(input1, input2, output_ptr))
-                },
+                }
                 2 => {
                     let (input1, input2, output_ptr) = self.get_full_opcode()?;
                     self.mem[output_ptr] = input1 * input2;
                     Ok(OPCODE::MULTI(input1, input2, output_ptr))
-                },
+                }
                 99 => {
                     return Ok(OPCODE::HALT);
                 }
                 _ => {
-                    let error_msg = format!("Unknown OP code '{}' at position {}.",
-                                            opcode, self.instr_ptr);
+                    let error_msg = format!(
+                        "Unknown OP code '{}' at position {}.",
+                        opcode, self.instr_ptr
+                    );
                     return Err(error_msg);
                 }
             };
@@ -49,13 +54,22 @@ impl IntcodeMachine {
             self.pc += 1;
             self.instr_ptr += 4;
             result
-
         } else {
             /* We somehow over indexed. Perhaps bad input? */
-            let error_msg = format!("Attempted to read opcode at position {} \
-                                     which does not exist!", self.instr_ptr);
+            let error_msg = format!(
+                "Attempted to read opcode at position {} \
+                 which does not exist!",
+                self.instr_ptr
+            );
             Err(error_msg)
         }
+    }
+
+    /* Resets the computer */
+    pub fn reset(&mut self) {
+        self.pc = 0;
+        self.mem = self.bin.clone();
+        self.instr_ptr = 0;
     }
 
     /* Returns the value of the INTCODE at the specified position */
@@ -68,21 +82,39 @@ impl IntcodeMachine {
         self.mem.clone()
     }
 
-    /* Runs through all the steps and returns the value at positin 0 */
+    /* Runs through all the steps and returns the value at position 0. Resets
+     * the machine when finished (but not on error).
+     */
     pub fn run(&mut self) -> Result<usize, String> {
         loop {
             match self.step() {
-                Ok(OPCODE::HALT) => return Ok(self.mem[0]),
+                Ok(OPCODE::HALT) => {
+                    self.reset();
+                    return Ok(self.mem[0]);
+                }
                 Err(msg) => return Err(msg),
-                Ok(code) => { println!("{:?}", code); } 
+                Ok(code) => {
+                    println!("{:?}", code);
+                }
             }
+        }
+    }
+
+    /* Like run() but allows you to set the first and second position */
+    pub fn run_with_inputs(&mut self, noun: usize, verb: usize) -> Result<usize, String> {
+        if self.mem.len() < 3 {
+            Err(String::from("Memory of insufficient lenght"))
+        } else {
+            self.mem[1] = noun;
+            self.mem[2] = verb;
+            self.run()
         }
     }
 
     /* Convenience Functions */
     fn get_full_opcode(&self) -> Result<(usize, usize, usize), String> {
         let start = self.instr_ptr + 1;
-        let end   = self.instr_ptr + 4;
+        let end = self.instr_ptr + 4;
         let opcode = self.mem.get(start..end);
 
         if let Some(opcode) = opcode {
@@ -91,15 +123,15 @@ impl IntcodeMachine {
             let output_ptr = opcode[2];
             Ok((input1, input2, output_ptr))
         } else {
-            let error_msg = format!("Attempted to grab arguments for opcode {} \
-                                     at position {} but arguments do not exist!",
-                                     self.pc, self.instr_ptr);
+            let error_msg = format!(
+                "Attempted to grab arguments for opcode {} \
+                 at position {} but arguments do not exist!",
+                self.pc, self.instr_ptr
+            );
             Err(error_msg)
-        
         }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -112,34 +144,38 @@ mod tests {
                 Ok(OPCODE::HALT) => {
                     assert_eq!(computer.get_state(), answer);
                     break;
-                },
+                }
                 Err(string) => {
                     println!("{}", string);
                     panic!();
-                },
-                Ok(code) => { println!("{:?}", code); } 
+                }
+                Ok(code) => {
+                    println!("{:?}", code);
+                }
             }
         }
-
     }
 
     #[test]
     fn test0() {
-        helper(vec![1,0,0,0,99], vec![2,0,0,0,99]);
+        helper(vec![1, 0, 0, 0, 99], vec![2, 0, 0, 0, 99]);
     }
 
     #[test]
     fn test1() {
-        helper(vec![2,3,0,3,99], vec![2,3,0,6,99]);
+        helper(vec![2, 3, 0, 3, 99], vec![2, 3, 0, 6, 99]);
     }
 
     #[test]
     fn test2() {
-        helper(vec![2,4,4,5,99,0], vec![2,4,4,5,99,9801]);
+        helper(vec![2, 4, 4, 5, 99, 0], vec![2, 4, 4, 5, 99, 9801]);
     }
 
     #[test]
     fn test3() {
-        helper(vec![1,1,1,4,99,5,6,0,99], vec![30,1,1,4,2,5,6,0,99]);
+        helper(
+            vec![1, 1, 1, 4, 99, 5, 6, 0, 99],
+            vec![30, 1, 1, 4, 2, 5, 6, 0, 99],
+        );
     }
 }
--- 2019/day-2/tests/day-2-test-1.rs
+++ 2019/day-2/tests/day-2-test-1.rs
@@ -1,24 +1,20 @@
 /* Our Crate */
-use day_2::{IntcodeMachine, OPCODE};
+use day_2::IntcodeMachine;
 
 /* Standard Library */
 use std::fs::File;
-use std::io::{self, prelude::*};
+use std::io::Read;
 
 #[test]
 /* Takes the input and calculates the final result of position 0 */
-fn calc_position_0() -> Result<(), String> {
-    let mut file = File::open("input.txt").map_err(|e| e.to_string())?;
+fn rebuild_intcode_computer() -> Result<(), String> {
+    /* Read and parse the file */
+    let mut file = File::open("input.txt").unwrap();
     let mut s = String::with_capacity(100);
-    file.read_to_string(&mut s).map_err(|e| e.to_string())?;
-    let mut input: Vec<usize> = s.split(",").filter_map(|s| s.parse().ok())
-                                            .collect();
+    file.read_to_string(&mut s).unwrap();
+    let input: Vec<usize> = s.split(",").filter_map(|s| s.parse().ok()).collect();
 
-    /* Set Noun and Verb */
-    input[1] = 12;
-    input[2] = 2;
-
-    let result = IntcodeMachine::new(input).run()?;
+    let result = IntcodeMachine::new(input).run_with_inputs(12, 2)?;
     println!("OPCODE @ POSITION 0: {}", result);
 
     Ok(())
