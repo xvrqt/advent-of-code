0fde6019eff164ee788687defdd47c07f6b71310
Amy Jie <git.xvrqt.com>
2019-12-06 10:47:21 -0800

    Updated the tests to read input more securely

--- 2019/day-2/src/lib.rs
+++ 2019/day-2/src/lib.rs
@@ -1,7 +1,7 @@
 /* Intcode Computer */
 pub struct IntcodeMachine {
     pc: usize,
-    bin: Vec<usize>,
+    mem: Vec<usize>,
     instr_ptr: usize,
 }
 
@@ -13,26 +13,26 @@ pub enum OPCODE {
 }
 
 impl IntcodeMachine {
-    pub fn new(bin: Vec<usize>) -> Self {
+    pub fn new(mem: Vec<usize>) -> Self {
         IntcodeMachine { 
             pc: 0,
-            bin,
+            mem,
             instr_ptr: 0,
         }
     }
 
-    /* Performs the next opcode instruction. Mutates the bin. */
+    /* Performs the next opcode instruction. Mutates the mem. */
     pub fn step(&mut self) -> Result<OPCODE, String> {
-        if let Some(opcode) = self.bin.get(self.instr_ptr) {
+        if let Some(opcode) = self.mem.get(self.instr_ptr) {
             let result = match opcode {
                 1 => {
                     let (input1, input2, output_ptr) = self.get_full_opcode()?;
-                    self.bin[output_ptr] = input1 + input2;
+                    self.mem[output_ptr] = input1 + input2;
                     Ok(OPCODE::ADD(input1, input2, output_ptr))
                 },
                 2 => {
                     let (input1, input2, output_ptr) = self.get_full_opcode()?;
-                    self.bin[output_ptr] = input1 * input2;
+                    self.mem[output_ptr] = input1 * input2;
                     Ok(OPCODE::MULTI(input1, input2, output_ptr))
                 },
                 99 => {
@@ -60,23 +60,23 @@ impl IntcodeMachine {
 
     /* Returns the value of the INTCODE at the specified position */
     pub fn get_intcode(&self, position: usize) -> Option<usize> {
-        self.bin.get(position).cloned()
+        self.mem.get(position).cloned()
     }
 
-    /* Returns a copy of the input */
+    /* Returns a copy of the memory */
     pub fn get_state(&self) -> Vec<usize> {
-        self.bin.clone()
+        self.mem.clone()
     }
 
     /* Convenience Functions */
     fn get_full_opcode(&self) -> Result<(usize, usize, usize), String> {
         let start = self.instr_ptr + 1;
         let end   = self.instr_ptr + 4;
-        let opcode = self.bin.get(start..end);
+        let opcode = self.mem.get(start..end);
 
         if let Some(opcode) = opcode {
-            let input1 = self.bin[opcode[0]];
-            let input2 = self.bin[opcode[1]];
+            let input1 = self.mem[opcode[0]];
+            let input2 = self.mem[opcode[1]];
             let output_ptr = opcode[2];
             Ok((input1, input2, output_ptr))
         } else {
