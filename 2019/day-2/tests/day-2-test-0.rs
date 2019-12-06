b6819e2335589aefa09a3dde7392dcbf7b8d1982
Amy Jie <git.xvrqt.com>
2019-12-06 11:15:53 -0800

    Refactored the tests/ to use the new IntCode machine capabilities and be less verbose.

--- 2019/day-2/tests/day-2-test-0.rs
+++ 2019/day-2/tests/day-2-test-0.rs
@@ -3,37 +3,23 @@ use day_2::{IntcodeMachine, OPCODE};
 
 /* Standard Library */
 use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
+use std::io::{self, prelude::*};
 
 #[test]
 /* Takes the input and calculates the final result of position 0 */
-fn calc_position_0() -> io::Result<()> {
-    let mut file = File::open("input.txt")?;
+fn calc_position_0() -> Result<(), String> {
+    let mut file = File::open("input.txt").map_err(|e| e.to_string())?;
     let mut s = String::with_capacity(100);
-    file.read_to_string(&mut s)?;
+    file.read_to_string(&mut s).map_err(|e| e.to_string())?;
     let mut input: Vec<usize> = s.split(",").filter_map(|s| s.parse().ok())
                                             .collect();
 
+    /* Set Noun and Verb */
     input[1] = 12;
     input[2] = 2;
 
-    let mut computer = IntcodeMachine::new(input);
-    loop {
-        match computer.step() {
-            Ok(OPCODE::HALT) => {
-                break;
-            },
-            Err(string) => {
-                println!("{}", string);
-                panic!();
-            },
-            Ok(code) => { println!("{:?}", code); } 
-        }
-    }
-
-    if let Some(pos_0) = computer.get_intcode(0) {
-        println!("OPCODE @ POSITION 0: {}", pos_0);
-    }
+    let result = IntcodeMachine::new(input).run()?;
+    println!("OPCODE @ POSITION 0: {}", result);
 
     Ok(())
 }
